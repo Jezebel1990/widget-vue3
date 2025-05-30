e7a860504be4b4c63dd461c70a3b13e5b1ff2257
Jezebel1990 <jezebelguedes@gmail.com>
2025-05-30 00:24:10 -0300

    feat: Creating a floating button that opens the Widget

--- src/views/Widget/Box.vue
+++ src/views/Widget/Box.vue
@@ -1,3 +1,95 @@
 <template>
-<h1>box</h1>
+  <div class="box animate__animated animate__fadeInUp animate__faster">
+  <div
+   :class="{
+     'justify-between': canShowAdditionalControlAndInfo,
+     'justify-end': !canShowAdditionalControlAndInfo
+    }"
+    class="relative w-full flex">
+    <button
+     v-if="canShowAdditionalControlAndInfo"
+     @click="() => ({})"
+     :disabled="canGoBack"
+     :class="{ invisible: canGoBack }"
+     class="text-xl text-gray-800 focus:outline-none"
+    >
+    <icon name="arrow-right" :color="colors.gray['800']"/>
+    </button>
+
+    <p
+     v-if="canShowAdditionalControlAndInfo"
+     class="text-xl font-black text-center text-brand-main"
+    >
+    {{ label }}
+    </p>
+
+    <button
+     @click="() => emit('close-box')"
+     class="text-xl text-gray-800 focus:outline-none"
+    >
+    <icon size="14" name="close" :color="colors.gray['800']"/>
+    </button>
+  </div>
+
+  wizard
+  <div class="text-gray-800 text-sm flex" v-if="canShowAdditionalControlAndInfo">
+   <icon name="chat" class="mr-1" :color="brandColors.graydark" />
+   widget by
+   <span class="ml-1 font-bold">feedbacker</span>
+  </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { brand } from '../../../palette'
+import Icon from '../../components/Icon/index.vue'
+import colors from 'tailwindcss/colors'
+import useStore from '../../hooks/store'
+
+export default defineComponent({
+  emits: ['close-box'],
+  components: { Icon },
+  setup (_, { emit }) {
+    const store = useStore()
+
+    const label = computed(() => {
+      switch (store.feedbackType) {
+        case 'ISSUE':
+          return 'Reporte um problema'
+        case 'IDEA':
+          return 'Nos fale a sua ideia'
+        case 'OTHER':
+          return 'Abra sua mente'
+        default:
+          return 'O que você tem em mente?'
+      }
+    })
+
+    const canGoBack = computed(() => {
+      return store.currentComponent === 'SelectFeedbackType'
+    })
+
+    const canShowAdditionalControlAndInfo = computed(() => {
+      return store.currentComponent !== 'Success' && store.currentComponent !== 'Error'
+    })
+
+    return {
+      emit,
+      canGoBack,
+      brandColors: brand,
+      label,
+      colors,
+      canShowAdditionalControlAndInfo
+    }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.box {
+  @apply fixed z-50 bottom-0 right-0 mb-5 mr-5 bg-white rounded-xl
+    py-5 px-5 flex flex-col items-center shadow-xl select-none;
+  width: 400px;
+}
+</style>
